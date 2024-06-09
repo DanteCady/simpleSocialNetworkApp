901dbd49d671b594a22456a5326ab8b657755437
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-06-09 18:15:30 -0400

    Removed unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import react from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
-import Home from './views/home.js';
 import Login from './views/auth/login';
 import Signup from './views/auth/signup';
 import Feed from './views/home/dashboard';
 import LikedActivity from './views/activity/activity_Likes';
-import PostsActivity from './views/activity/activity_Posts';
+import PostsActivity from './views/activity/activity_posts';
 
 const App: React.FC = () => (
 	<UserProvider>
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" Component={Login} />
-				<Route path="/home" Component={Home} />
 				<Route path="/login" Component={Login} />
 				<Route path="/signup" Component={Signup} />
 				<Route path="/feed" Component={Feed} />

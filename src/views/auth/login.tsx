f0413a9438d83c36b4191c19e93c3f6a533c8f67
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-06-09 15:18:14 -0400

    Added login status in login

--- src/views/auth/login.tsx
+++ src/views/auth/login.tsx
@@ -5,68 +5,84 @@ import axios from 'axios';
 import BoltIcon from '@mui/icons-material/Bolt';
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [loginStatus, setLoginStatus] = useState('');
+	const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:3001/login', {
-        username: username,
-        password: password,
-      });
-      navigate('/feed');
-    } catch (error) {
-      console.error('There was an error logging in!', error);
-    }
-  };
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			const response = await axios.post('http://localhost:3001/login', {
+				username: username,
+				password: password,
+			});
+			console.log('Login response:', response);
+			if (response.data.success) {
+				localStorage.setItem('username', username);
+			} else {
+				setLoginStatus('Invalid username or password');
+			}
 
-  return (
-    <Container maxWidth="sm" style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <Card>
-        <CardContent>
-		<Typography variant="h4" component="h1" align="center" gutterBottom>SSSN APP
-		<BoltIcon style={{ fontSize: 40, color: '#F8D53B' }} />
-		</Typography>
-          <Typography variant="h4" component="h1" align="center" gutterBottom>Login</Typography>
-		  <Typography variant="body1" align="center" gutterBottom>Enter your username and password</Typography>
-          <form onSubmit={handleSubmit}>
-            <TextField
-              label="Username"
-              variant="outlined"
-              fullWidth
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-              margin="normal"
-            />
-            <TextField
-              label="Password"
-              variant="outlined"
-              fullWidth
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              margin="normal"
-            />
-			<Typography variant="body1" align="center" gutterBottom>Don't have an account? <a href="/signup">Sign up</a></Typography>
-			<Typography variant="body1" align="center" gutterBottom>Forgot your password? <a href="/forgot">Reset</a></Typography>
-		    <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              fullWidth
-              style={{ marginTop: '16px' }}
-            >
-              Submit
-            </Button>
-          </form>
-        </CardContent>
-      </Card>
-    </Container>
-  );
+			navigate('/feed');
+		} catch (error) {
+			console.error('There was an error logging in!', error);
+		}
+	};
+
+	return (
+		<Container maxWidth="sm" style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+			<Card>
+				<CardContent>
+					<Typography variant="h4" component="h1" align="center" gutterBottom>
+						SSSN APP
+						<BoltIcon style={{ fontSize: 40, color: '#F8D53B' }} />
+					</Typography>
+					<Typography variant="h4" component="h1" align="center" gutterBottom>
+						Login
+					</Typography>
+					<Typography variant="body1" align="center" gutterBottom>
+						Enter your username and password
+					</Typography>
+					<form onSubmit={handleSubmit}>
+						<TextField
+							label="Username"
+							variant="outlined"
+							fullWidth
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							required
+							margin="normal"
+						/>
+						<TextField
+							label="Password"
+							variant="outlined"
+							fullWidth
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required
+							margin="normal"
+						/>
+						{loginStatus && (
+							<Typography variant="body1" align="center" gutterBottom>
+								{loginStatus}
+							</Typography>
+						)}
+						<Typography variant="body1" align="center" gutterBottom>
+							Don't have an account? <a href="/signup">Sign up</a>
+						</Typography>
+						<Typography variant="body1" align="center" gutterBottom>
+							Forgot your password? <a href="/forgot">Reset</a>
+						</Typography>
+						<Button type="submit" variant="contained" color="primary" fullWidth style={{ marginTop: '16px' }}>
+							Submit
+						</Button>
+					</form>
+				</CardContent>
+			</Card>
+		</Container>
+	);
 };
 
 export default Login;
